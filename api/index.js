74e15b06171bfcce0a1a2135688d5986f51979e1
matiasgonz1653 <96365031+matiasgonz1653@users.noreply.github.com>
2022-04-26 21:34:21 -0300

    heroku

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@
 const server = require("./src/app");
 const { conn } = require("./src/database/db");
 require("dotenv").config();
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
